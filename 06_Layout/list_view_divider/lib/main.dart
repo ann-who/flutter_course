799874d4b486aa32ea68ae1ca019542dd8c73554
Skillbox <it@skillbox.ru>
2021-05-20 12:20:46 +0000

    Add lesson

---
+++ 06_Layout/list_view_divider/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var listData = List.generate(50, (index) => index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'List view divider',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ 06_Layout/text_field_styled/android/app/src/main/kotlin/com/example/text_field_styled/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.text_field_styled
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
