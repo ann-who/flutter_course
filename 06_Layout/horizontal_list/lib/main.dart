74bbdbf9c8bf3d36fe20c050a024b97b8c884a2d
Ann Impalovskaya <covertover@gmail.com>
2021-05-26 20:09:21 +0300

    Add align

--- 06_Layout/horizontal_list/lib/main.dart
+++ 06_Layout/horizontal_list/lib/main.dart
@@ -27,7 +27,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final int _maxWidth = 500;
-  final int _maxHeight = 100;
   final int _itemCount = 30;
   @override
   Widget build(BuildContext context) {
@@ -37,27 +36,26 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(
         children: <Widget>[
-          Container(
-            constraints: BoxConstraints(),
+          Expanded(
             child: LayoutBuilder(
                 builder: (BuildContext context, BoxConstraints constraints) {
               return ListView.builder(
                   itemCount: _itemCount,
                   itemBuilder: (BuildContext context, int index) {
-                    var card = Card(
-                      color: Colors.amber,
-                      child: Center(child: Text('$index')),
-                    );
-                    return Container(
-                      width: constraints.maxWidth <= _maxWidth ? 100 : 100,
-                      height: constraints.maxWidth <= _maxWidth ? 100 : 100,
-                      child: card,
+                    return Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                        width: constraints.maxWidth <= _maxWidth
+                            ? 100
+                            : double.maxFinite,
+                        height: 100,
+                        child: Card(
+                          color: Colors.amber,
+                          child: Center(child: Text('$index')),
+                        ),
+                      ),
                     );
                   },
-
-                  // padding: constraints.maxWidth <= _maxWidth
-                  //     ? EdgeInsets.only(bottom: 350)
-                  //     : EdgeInsets.zero,
                   scrollDirection: constraints.maxWidth <= _maxWidth
                       ? Axis.horizontal
                       : Axis.vertical);
