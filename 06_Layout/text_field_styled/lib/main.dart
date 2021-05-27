52d390e4bca18e2b8ae8b38f0772a53b0b6ba3c6
Ann Impalovskaya <covertover@gmail.com>
2021-05-27 20:19:30 +0300

    Customise text field

--- 06_Layout/text_field_styled/lib/main.dart
+++ 06_Layout/text_field_styled/lib/main.dart
@@ -25,6 +25,8 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+final searchFieldStyle = TextStyle(color: Colors.purple[400]);
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
@@ -34,9 +36,37 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            TextField(),
+            Theme(
+              data: ThemeData(
+                primaryColor: Colors.purple[400],
+                hintColor: Colors.grey[500],
+              ),
+              child: Container(
+                padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.purple[400], width: 2),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(18.0)),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(18.0)),
+                    ),
+                    labelText: 'Search',
+                    hintText: 'Введите значение',
+                    suffixIcon: Icon(
+                      Icons.search,
+                    ),
+                    helperText: 'Поле для поиска заметок',
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
