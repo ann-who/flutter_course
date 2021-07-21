6c3528193170c2c4e6aff0e8969247d6a7704e6a
Ann Impalovskaya <covertover@gmail.com>
2021-07-21 14:59:09 +0300

    Add TextField searching

--- 08_AsyncFuture/load_file/lib/main.dart
+++ 08_AsyncFuture/load_file/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:load_file/fetch_file.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,20 +29,104 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _textController = TextEditingController();
+  Future<String> _screenText;
+
   @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              IntrinsicHeight(
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 3,
+                        child: TextField(
+                          controller: _textController,
+                          expands: false,
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.black, width: 2),
+                              borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(8.0),
+                                  bottomLeft: Radius.circular(8.0)),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.black, width: 2),
+                              borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(8.0),
+                                  bottomLeft: Radius.circular(8.0)),
+                            ),
+                            hintText: 'Enter file name',
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(8.0),
+                              bottomRight: Radius.circular(8.0),
+                            ),
+                          ),
+                          child: TextButton(
+                            onPressed: () {
+                              _screenText = fetchFileFromAssets(
+                                  'assets/' + _textController.text + '.txt');
+                              setState(() {});
+                            },
+                            child: Text(
+                              'Найти',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(15.0),
+                child: FutureBuilder<String>(
+                  future: _screenText,
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.done:
+                        if (snapshot.hasError) {
+                          print(snapshot.error);
+                          return const Center(child: Text('File not found'));
+                        }
+                        return SingleChildScrollView(
+                            child: Text(snapshot.data));
+                      case ConnectionState.waiting:
+                        return const Center(child: Text('Loading...'));
+                      default:
+                        return const Center(child: Text('Press search button'));
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
