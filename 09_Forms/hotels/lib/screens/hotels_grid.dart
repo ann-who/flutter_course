fe25d08a11404a95a2d7761483977bc554fac56c
Ann Impalovskaya <covertover@gmail.com>
2022-05-31 17:57:45 +0300

    Revert "Revert "refactoring""
    This reverts commit c9769bbfeb86bb73aae8c98d69b558494283212f.

--- 09_Forms/hotels/lib/screens/hotels_grid.dart
+++ 09_Forms/hotels/lib/screens/hotels_grid.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hotels/components/hotel_card.dart';
 import 'package:hotels/models/hotel.dart';
-import 'package:hotels/resources/theme.dart';
 
 class HotelsGridView extends StatelessWidget {
   final List<Hotel> hotels;
@@ -14,47 +14,7 @@ class HotelsGridView extends StatelessWidget {
       crossAxisCount: 2,
       children: <Widget>[
         ...hotels.map((hotel) {
-          return Card(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  width: double.maxFinite,
-                  height: 100,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(AppConstants.cardRadius),
-                      topRight: Radius.circular(AppConstants.cardRadius),
-                    ),
-                    child: Image(
-                      image: AssetImage('assets/images/${hotel.poster}'),
-                      fit: BoxFit.fitWidth,
-                    ),
-                  ),
-                ),
-                Spacer(),
-                Text(hotel.name, textAlign: TextAlign.center),
-                Spacer(),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    minimumSize: MaterialStateProperty.all(
-                      Size(
-                        200,
-                        50,
-                      ),
-                    ),
-                  ),
-                  onPressed: () {
-                    Navigator.pushNamed(
-                      context,
-                      '/detailsInfo',
-                      arguments: hotel.uuid,
-                    );
-                  },
-                  child: Text('Подробнее'),
-                ),
-              ],
-            ),
-          );
+          return HotelGridCard(hotel: hotel);
         }).toList(),
       ],
     );
